8d3689500a491c70ea5ca3a6daa33d288f779140
munarin10 <erick.munarin@gmail.com>
2024-09-06 20:34:35 -0300

    Inserindo método get

--- src/main/java/com/api/venda/controllers/UserController.java
+++ src/main/java/com/api/venda/controllers/UserController.java
@@ -1,5 +1,31 @@
 package com.api.venda.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.api.venda.models.User;
+import com.api.venda.services.UserService;
+
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = userService.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = this.userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
+
