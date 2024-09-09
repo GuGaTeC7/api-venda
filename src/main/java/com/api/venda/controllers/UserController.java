2ae9b047ccc1ee0ee0f4c5f2f88fc1b4dba39b80
Andson22818505 <Andsoneloidasilva@gmail.com>
2024-09-09 18:59:35 -0300

    inserindo atualizacao de usuario

--- src/main/java/com/api/venda/controllers/UserController.java
+++ src/main/java/com/api/venda/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -36,5 +37,11 @@ public class UserController {
         this.userService.create(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody User obj) {
+        this.userService.update(obj);
+        return ResponseEntity.noContent().build();
     }
 }
